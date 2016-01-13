aead2b92cfd6e8743ab82bdfefcca70141acf8a5
gurbuza <adem.gurbuz@iut-valence.fr>
2016-01-13 09:23:11 +0100

    First commit. Nothing particulary, juste the link with github.

---
+++ embeddedApplication/embeddedApplication.ino
@@ -0,0 +1,22 @@
+float t ;
+IMUData imuData;
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  
+  if (fuseeAuSol()){
+    ring();
+  }
+  
+  t = getTime();               // millis()
+  imuData = getIMUData();
+  char row[90];                  // t+Gx+Gy+Gz+Az+Ay+Az+Mx+My+Mz+B (t+Gyro+Accele+Magnet+Baro)    
+  sprintf(row, "%.3f", t);   
+  for (i = 0; i<size()) 
+  
+
+}
