8f3019b5f61f1b1947467d404c10cfb1639831c6
gurbuza <adem.gurbuz@iut-valence.fr>
2016-01-14 10:19:17 +0100

    Class IMUData .h/.cpp not finish, need XYZData class

---
+++ embeddedApplication/IMUData.cpp
@@ -0,0 +1,16 @@
+#include "IMUData.h"
+
+IMUData::IMUData(XYZData acceleration, XYZData gyroscope, 
+        XYZData magnetism, float a_pressure)
+{
+    accelerationX = acceleration.getX();
+    accelerationY = acceleration.getY();
+    accelerationZ = acceleration.getZ();
+    gyroscopeX = gyroscope.getX();
+    gyroscopeY = gyroscope.getY();
+    gyroscopeZ = gyroscope.getZ();
+    magnetismX = magnetism.getX();
+    magnetismY = magnetism.getY();
+    magnetismZ = magnetism.getZ();
+    pressure = pressure;  
+}
---
+++ embeddedApplication/IMUData.h
@@ -0,0 +1,26 @@
+#ifndef IMUData_h
+#define IMUData_h
+
+
+class IMUData
+{
+  private:
+  
+    float accelerationX;
+    float accelerationY;
+    float accelerationZ;
+    float gyroscopeX;
+    float gyroscopeY;
+    float gyroscopeZ;
+    float magnetismX;
+    float magnetismY;
+    float magnetismZ;
+    float pressure;
+    
+  public:
+  // XYZData a modifier
+    IMUData(XYZData acceleration, XYZData gyroscope, 
+        XYZData magnetism, float a_pressure)
+}
+
+#endif
